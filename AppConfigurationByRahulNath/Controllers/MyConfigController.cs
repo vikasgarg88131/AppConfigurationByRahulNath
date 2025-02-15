ae8463632652d3ebaac18b0518045461c668690f
Vikas Garg <vikasgarg88131@outlook.com>
2025-02-15 13:49:23 +0530

    Add project files.

---
+++ AppConfigurationByRahulNath/Controllers/MyConfigController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IDictionary<string, string?> Get()
+        {
+            return _configuration.GetSection("Demo")
+                          .GetChildren()
+                          .ToDictionary(a => a.Key, a => a.Value);
+
+        }
+    }
+}
